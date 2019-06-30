bc1a8d758bed232ee67638e200366ec7af9d2701
andrewsuhanow <andrewsuhanow@gmail.com>
2019-06-30 20:38:36 +0300

    main page, base

--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -14,32 +14,36 @@
 
 
 
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Fixed navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            <form class="form-inline mt-2 mt-md-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        {{--<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>--}}
+        <input class="form-control main_input w-100" type="text" placeholder="Enter link" aria-label="Search">
+        <ul class="navbar-nav px-3">
+            <li class="nav-item text-nowrap">
+                <a class="nav-link" href="#">Sign out</a>
+            </li>
+        </ul>
     </nav>
 
 
+    <div class="container-fluid " >
+        <div class="row">
+                <div class="categories_block">
+                    <div  >
+                        <div class="jumbotron shadow p-3 categories_sub_block">
+                            <h1>Name</h1>
+                            <a class="lead"> <b>Link:</b> "This example is a quick exercise to illustrate how the top-aligned navbar works. As you scroll, this navbar remains in its original position and moves with the rest of the page."</a>
+                        </div>
+                    </div>
+                    <div  >
+                        <div class="jumbotron shadow p-3 categories_sub_block">
+                            <h1>Name</h1>
+                            <a class="lead"> <b>Link:</b> "This example is a quick exercise to illustrate how the top-aligned navbar works. As you scroll, this navbar remains in its original position and moves with the rest of the page."</a>
+                        </div>
+                    </div>
 
+                </div>
+        </div>
+    </div>
 
 @stop
 
@@ -48,20 +52,18 @@
 
     <style>
 
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
+        .categories_block{
+            /*position: fixed;*/
+            background: #84c183;
+            min-height: 100vh;
+            /*width: 100%;*/
+            padding: 50px;
         }
 
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
+        .categories_sub_block{
+            background: #006514;
         }
+
 
     </style>
 
